5e2f0874dce3afb8a66a93facbf928cb84594282
PinkPanther-ny <alvincny529@gmail.com>
2022-03-29 15:03:27 +0800

    The should only run once to re structure data directory, project-specific

--- change_image_dir.py
+++ change_image_dir.py
@@ -1,41 +1,20 @@
-# import os
-
-# directory = "./train_images/"
-
-# if not os.path.exists("./data/"):
-#     os.makedirs("./data/")
-    
-# for root, subdirectories, files in os.walk(directory):
-#     for subdirectory in subdirectories:
-#         # print(os.path.join(root, subdirectory))
-#         pass
-#     for file in files:
-#         class_name = file.split("__")[0]
-#         new_dir = "./data/" + class_name
-#         if not os.path.exists(new_dir):
-#             os.makedirs(new_dir)
-            
-#         os.rename(os.path.join(root, file), new_dir + "/" + file)
-#         print(os.path.join(root, file))
-#         pass
-
-import os
-import csv
-import random
 import os
 
-# Generate submission
+directory = "./train_images/"
 
-header = ['Id', 'Predicted']
-directory = "./test_images/"
-
+if not os.path.exists("./data/"):
+    os.makedirs("./data/")
     
 for root, subdirectories, files in os.walk(directory):
     for subdirectory in subdirectories:
         # print(os.path.join(root, subdirectory))
         pass
     for file in files:
-        class_name = int(file.split("-")[-1].split('.')[0])
-        file_name = os.path.join(root, file)
-        print(file_name, class_name)
+        class_name = file.split("__")[0]
+        new_dir = "./data/" + class_name
+        if not os.path.exists(new_dir):
+            os.makedirs(new_dir)
+            
+        os.rename(os.path.join(root, file), new_dir + "/" + file)
+        print(os.path.join(root, file))
         pass
