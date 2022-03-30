92adc1576594e45520f0da1e6eb97381e842e0a5
PinkPanther-ny <alvincny529@gmail.com>
2022-03-30 10:15:30 +0800

    Add comment and remove magic variable

--- src/utils/change_image_dir.py
+++ src/utils/change_image_dir.py
@@ -1,9 +1,13 @@
+# This should only run one time to transform kaggle competition's data folder 
+# structure to the type that can be accepted by ImageFolder
+
 import os
+from ..settings import configs
 
 directory = "./train_images/"
 
-if not os.path.exists("./data/"):
-    os.makedirs("./data/")
+if not os.path.exists(configs._DATA_DIR):
+    os.makedirs(configs._DATA_DIR)
     
 for root, subdirectories, files in os.walk(directory):
     for subdirectory in subdirectories:
@@ -11,7 +15,7 @@ for root, subdirectories, files in os.walk(directory):
         pass
     for file in files:
         class_name = file.split("__")[0]
-        new_dir = "./data/" + class_name
+        new_dir = configs._DATA_DIR + class_name
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
             
