6fd4d21b0804b21a9a0afbfc4b81916ff1172b8b
nuoyanc <alvincny529@gmail.com>
2022-03-30 23:20:50 -0400

    Implement configurable input with default value

--- run.sh
+++ run.sh
@@ -5,11 +5,12 @@ MAIN_ENTRY="./main.py"
 export CUDA_VISIBLE_DEVICES="0,1,2,3"
 export OMP_NUM_THREADS=2
 
+CONFIG_FILE=${1:-"config.json"}
 
-echo ""
-echo -e "\033[43;44m USING $USE_N_GPU GPUs TO TRAIN \033[0m"
+echo -e "\033[104;097m USING $USE_N_GPU GPUs TO TRAIN \033[0m"
 
+echo -e "\033[104;097m LOADING \033[090;102m$CONFIG_FILE \033[0m \033[0m"
 
 # -f flag for specify configuration file location
 # -s flag for specify whether to save a copy of current default configuration
-python -m torch.distributed.run --nproc_per_node $USE_N_GPU $MAIN_ENTRY -s True -f config.json
+python -m torch.distributed.run --nproc_per_node $USE_N_GPU $MAIN_ENTRY -s True -f $CONFIG_FILE
