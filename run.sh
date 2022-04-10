a792f8f3f746fb09e8433fa73362b46d9d152357
PinkPanther-ny <alvincny529@gmail.com>
2022-04-10 15:19:32 +0800

    Minor change

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MAIN_ENTRY="./main.py"
-export CUDA_VISIBLE_DEVICES="0,1,2,3"
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 export OMP_NUM_THREADS=2
 
 CONFIG_FILE=${1:-"default.json"}
