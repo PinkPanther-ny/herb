9ca50c4bc1d789347b9dbdc950db4e1cdc2e6fdf
nuoyanc <alvincny529@gmail.com>
2022-04-13 01:52:02 -0400

    Add accimage, speed up image loading by 2x, Add dataset index csv file, load from csv if exist

--- src/preprocess/dataset/herb_dataset.py
+++ src/preprocess/dataset/herb_dataset.py
@@ -1,10 +1,12 @@
 import os
+import csv
 import re
+import pandas as pd
 from typing import Any, Callable, Dict, Optional
 from PIL import Image
 from torch.utils.data.dataset import Dataset, Subset
 
-from ...settings import logger
+from ...settings import logger, configs
 
 
 class HerbDatasetIniError(Exception):
@@ -21,23 +23,30 @@ def pil_loader(path: str) -> Image.Image:
 def accimage_loader(path: str) -> Any:
     import accimage
     try:
-        return accimage.Image(path)
+        acc_loader = accimage.Image(path)
+        # logger.info("Using accimage loader")
+        return acc_loader
     except IOError:
         # Potentially a decoding problem, fall back to PIL.Image
-        return pil_loader(path)
+        pil = pil_loader(path)
+        # logger.info("Using pil loader")
+        return pil
 
 
 def default_loader(path: str) -> Any:
-    from torchvision import get_image_backend
+    from torchvision import get_image_backend, set_image_backend
+    set_image_backend("accimage")
     if get_image_backend() == 'accimage':
         return accimage_loader(path)
     else:
+        # logger.info("Using pil loader")
         return pil_loader(path)
 
 
 class HerbDataset(Dataset):
 
-    regex_filename: str = '\d{5}__\d{3}.jpg'
+    regex_image_filename: str = '\d{5}__\d{3}.jpg'
+    _herb_csv_filename:str = 'herbIndex.csv'
     
     def __init__(self,
                  root: str,
@@ -45,18 +54,31 @@ class HerbDataset(Dataset):
                  loader: Callable[[str], Any] = default_loader,
                  ):
         if not os.path.isdir(root):
-            msg = f"Fatal!{root} is not a directory!"
+            msg = f"Fatal! {root} is not a directory!"
             logger.critical(msg)
             raise HerbDatasetIniError(msg)
         self.root = root
-        classes, class_to_idx = self.find_classes(self.root)
-        
-        self.classes = classes
-        self.class_to_idx = class_to_idx
         self.transform = transform
-        
-        self.samples = self.make_dataset(self.root, self.class_to_idx)
         self.loader = loader
+        
+        index_file_exist = os.path.isfile(root + self._herb_csv_filename)
+        
+        if not index_file_exist:
+            classes, class_to_idx = self.find_classes(self.root)
+            self.classes = classes
+            self.samples = self.make_dataset(self.root, class_to_idx)
+            
+            if configs._LOCAL_RANK == 0:
+                idx_to_class = {y:x for x,y in class_to_idx.items()}
+                self.save_index(idx_to_class)
+                # Store class name as string instead of casting to int
+                self.df = pd.read_csv(root + self._herb_csv_filename, dtype={1:'object'})
+        else:
+                # Store class name as string instead of casting to int
+            self.df = pd.read_csv(root + self._herb_csv_filename, dtype={1:'object'})
+            self.classes = self.df['class_names'].unique().tolist()
+            self.samples = [(path, label) for path, label in zip(self.df['paths'], self.df['labels'])]
+            logger.info(f"Dataset loaded from index file: {self.root + self._herb_csv_filename}")
         
 
     def find_classes(self, directory: str):
@@ -64,15 +86,17 @@ class HerbDataset(Dataset):
         for root, _, files in os.walk(directory):
             
             for file in files:
-                if re.match(self.regex_filename, file):
+                if file == self._herb_csv_filename:
+                    continue
+                
+                if re.match(self.regex_image_filename, file):
                     classes.add(file.split("__")[0])
-                    # self.samples.append(os.path.join(root, file))
                 else:
                     msg = f"Dataset contains an invalid file {os.path.join(root, file)}!"
                     logger.critical(msg)
                     raise HerbDatasetIniError(msg)
         classes = sorted(list(classes))
-        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+        class_to_idx = {str(cls_name): i for i, cls_name in enumerate(classes)}
 
         return classes, class_to_idx
         
@@ -86,7 +110,10 @@ class HerbDataset(Dataset):
         for root, _, files in os.walk(directory):
             
             for file in files:
-                if re.match(self.regex_filename, file):
+                if file == self._herb_csv_filename:
+                    continue
+                
+                if re.match(self.regex_image_filename, file):
                     class_name = file.split("__")[0]
                     path_label_pairs.append((os.path.join(root, file), class_to_idx[class_name]))
                 else:
@@ -95,6 +122,19 @@ class HerbDataset(Dataset):
                     raise HerbDatasetIniError(msg)
         
         return sorted(path_label_pairs)
+
+    def save_index(self, idx_to_class):
+        
+        header = ['labels', 'class_names', 'paths']
+        with open(self.root + self._herb_csv_filename, 'w') as f:
+            writer = csv.writer(f)
+            # write the header
+            writer.writerow(header)
+            
+            for path, label in self.samples:
+                class_name = idx_to_class[label]
+                writer.writerow([label, class_name, path])
+        logger.info(f"Dataset index file not found, saved to {self.root + self._herb_csv_filename}")
 
     def __getitem__(self, index: int) -> Any:
         
